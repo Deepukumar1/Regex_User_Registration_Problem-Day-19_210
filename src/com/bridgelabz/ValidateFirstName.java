478c565939c698ab3352c2c2d1371b936653fb31
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-09 14:28:20 +0530

    welcom to rejex user program

---
+++ src/com/bridgelabz/ValidateFirstName.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class ValidateFirstName {
+	 public static void main(String[] args) {
+	        Validation validation=new Validation();
+	        validation.checkFirstName();
+
+	    }
+}
