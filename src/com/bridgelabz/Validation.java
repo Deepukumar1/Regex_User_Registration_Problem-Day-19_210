3e01abdaf51f90a85b14aa1276c22755ef1e9168
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-11-10 13:44:23 +0530

    Added: validate email

--- src/com/bridgelabz/Validation.java
+++ src/com/bridgelabz/Validation.java
@@ -17,14 +17,25 @@ public class Validation {
 	    }
 	    
 		public void checkLastName() {
-			 System.out.println("-----------------");
+			 System.out.println("************");
 		        System.out.println("Enter Last Name : ");
 		        String lastName = sc.next();
 		        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", lastName)) {
-		            System.out.println("LastName is Valid---!!");
+		            System.out.println("LastName is Valid  ");
 		        } else {
-		            System.out.println("LastName is Invalid--!!");
+		            System.out.println("LastName is Invalid   ");
 		        }
 			
 		}
+		
+		 public void checkEmail() {
+		        System.out.println("************");
+		        System.out.println("Enter Email : ");
+		        String email = sc.next();
+		        if (Pattern.matches("^[a-zA-Z]{3,}[.]?[A-za-z0-9]{3,}[.]?[a-zA-Z]+[@]{1}[a-z]+[.]?[a-z]*", email)) {
+		            System.out.println("Email is Valid  ");
+		        } else {
+		            System.out.println("Email is Invalid  ");
+		        }
+		    }
 }
